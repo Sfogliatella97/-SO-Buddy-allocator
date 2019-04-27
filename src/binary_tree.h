7d2354333f666d7b20aaa34c601200a240e3d51c
Sfogliatella97 <andreamusar@gmail.com>
2019-04-27 21:32:03 +0200

    Added function b_tree_memrequired. Further documented bin_tree.h

--- src/binary_tree.h
+++ src/binary_tree.h
@@ -1,4 +1,8 @@
-#define is_even(n) ((n == ((n >> 1) << 1))? 1 : 0)
+/*
+    Macros to navigate the binary tree
+*/
+
+#define is_even(n) (n == ((n >> 1) << 1))
 
 #define parent_index(index) ((((is_even(index+1))? (index + 1 ) : (index)) << 1) -1)
 
@@ -6,12 +10,34 @@
 
 #define left_child_index(index) (right_child_index(index) -1)
 
+/*
+    A binary tree with unsigned keys and int values.
+    It should be managed with functions within this header and not be
+    messed with otherwise
+*/
+
 typedef void b_tree;
 
-//Initializes a binary tree with as many levels as possible
+/*
+    The memory (in bytes) required to store a tree of <levels> number of levels,
+    where 0 means only the root, 1 means the root and its 2 children,
+    3 means the root, its 2 children and their 4 children, and so on
+*/
+unsigned b_tree_memrequired(unsigned size);
+
+/*
+    Initializes a binary tree with as many levels as possible
+*/
+
 b_tree* b_tree_init(void* mem, unsigned length);
 
-//Puts <val> in the tree at index <index>
+/*
+    Puts <val> at index <index>
+*/
 void b_tree_put(b_tree* tree, unsigned index, int val);
+
+/*
+    Reads the value at index <index>
+*/
 
 int b_tree_get(b_tree* tree, unsigned index);
