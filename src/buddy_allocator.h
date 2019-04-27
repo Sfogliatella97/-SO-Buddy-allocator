1654427faa6fd67f44b2eb90c72fa182d24b10bd
Sfogliatella97 <andreamusar@gmail.com>
2019-04-28 01:27:49 +0200

    Almost complete

--- src/buddy_allocator.h
+++ src/buddy_allocator.h
@@ -1,10 +1,39 @@
+#define buddy_allocator_b_tree_address(allocator) (allocator + sizeof(buddy_allocator))
+
 /*
-#define buddy_allocator_size(buffer_size)  TODO
+    Given an amount (<buffer_size>) of memory (<buffer>) to manage, we do it as follows:
+        when required to allocate a certain <amount> of memory, we divide <buffer> in 2^level
+        parts, where level is such that buffer / 2^(level + 1) < <amount> and level <= <levels>.
+*/
+
+typedef struct buddy_allocator_s {
+    char* buffer;
+    unsigned buffer_size;
+    unsigned levels;
+} buddy_allocator;
+
 /*
+    The amount of memory (in bytes) needed for the optimal functioning of the buddy allocator.
+    You should use this function to determine how much memory you should give to buddy_allocator_init
+    as <working_memory>.
+
+    The typical initialization of this allocator should be:
+
+        char buffer[buffer_size];
+
+        unsigned mem_required = buddy_allocator_memrequired(buffer_size, min_bucket_size);
+
+        char working_memory[mem_required];
+
+        buddy_allocator allocator = buddy_allocator_init(working_memory, mem_required
+                                                         buffer, buffer_size, min_bucket_size);
+*/
+
+unsigned buddy_allocator_memrequired(unsigned buffer_size, unsigned min_bucket_size);
 
 /* Initializes the buddy allocator with the following parameters:
      
-    -buddy_allocator : needs an empty buffer (the size of which can be computed
+    -working_memory : needs an empty buffer (the size of which can be computed
                        via the macro above) to fill with management structures
 
     -buffer : the actual memory to be managed
@@ -15,11 +44,21 @@
 
    The returned integer stands for:
 
+   -2147483647 : the working_memory_length was not enough to contain fundamental structs:
+        the allocator will not work (segmentation fault, or worse, will occur if you try to use it) 
+
     0 : all good
 
-    TODO
+        //NEED FURTHER EXPLANATIONS
+    1 : the working_memory_length was not enough to contain the tree:
+        the allocator will work, but there won't be as many "little chunks" as required
+    
+    //TODO
+
+    (the error code is actually a mask of flags such that: if the most significant bit is 1, the error is
+     beyond repair; if it's 0, it works but with some hindrances )
 */
-int buddy_allocator_init(void* buddy_allocator,
+int buddy_allocator_init(void* working_memory, unsigned working_memory_length,
                          char* buffer, unsigned buffer_size,
                          unsigned min_bucket_size);
 
@@ -27,4 +66,4 @@ int buddy_allocator_init(void* buddy_allocator,
 void* buddy_allocator_malloc(buddy_allocator* allocator, unsigned size);
 
 //Nothing to say here
-void* buddy_allocator_free(buddy_allocator* allocator, void* mem);
+void buddy_allocator_free(buddy_allocator* allocator, void* address);
