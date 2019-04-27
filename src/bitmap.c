4e899fd6116bc5b8d0606a1f9698a286e98048cf
Sfogliatella97 <andreamusar@gmail.com>
2019-04-27 21:36:01 +0200

    Bitmap implementation of b_tree_memrequired. Further commented

--- src/bitmap.c
+++ src/bitmap.c
@@ -2,6 +2,15 @@
 
 typedef char bitmap;
 
+unsigned bitmap_memrequired(unsigned size)
+{
+    unsigned base = size >> 3;
+    if(size == (base << 3))
+        return base;
+    else 
+        return (base + 1);
+}
+
 bitmap* bitmap_init(char* mem, unsigned length)
 {
     unsigned u;
@@ -10,7 +19,7 @@ bitmap* bitmap_init(char* mem, unsigned length)
     return (bitmap*) mem;
 }
 
-void bitmap_put(bitmap* map, char index, int val)
+void bitmap_put(bitmap* map, unsigned index, int val)
 {
     unsigned base = (index >> 3);
     unsigned offset = (index - (base << 3));
@@ -33,11 +42,21 @@ int bitmap_get(bitmap* map, unsigned index)
     return (res == MASK);
 }
 
+/*
+    We use the b_tree as a layer of abstraction and call functions in binary_tree.h.
+    If we compile buddy_allocator.c with bitmap.c (this file), we are actually using a bitmap,
+    but buddy_allocator doesn't need to know that. So we wrap functions, hoping that the compiler
+    will optimize it away.
+*/
+
+unsigned b_tree_memrequired(unsigned size)
+{ return bitmap_memrequired(size);}
+
 b_tree* b_tree_init(void* mem, unsigned length) 
-{ (b_tree*) bitmap_init(mem, length); }
+{ return (b_tree*) bitmap_init(mem, length); }
 
 void b_tree_put(b_tree* tree, unsigned index, int val)
 { bitmap_put((bitmap*) tree, index, val); }
 
 int b_tree_get(b_tree* tree, unsigned index)
-{ bitmap_get((bitmap*) tree, index); }
+{ return bitmap_get((bitmap*) tree, index); }
